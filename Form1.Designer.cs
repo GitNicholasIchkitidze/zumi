b1c42f6566d447a1b13eac2b47ed591f0a09ad31
GitNicholasIchkitidze <54753274+GitNicholasIchkitidze@users.noreply.github.com>
2019-10-07 01:49:41 +0400

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace Zumi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.TextH = new System.Windows.Forms.TextBox();
+            this.TextW = new System.Windows.Forms.TextBox();
+            this.TextY = new System.Windows.Forms.TextBox();
+            this.TextX = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Player1_cBoxScreen = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.panel1.Controls.Add(this.TextH);
+            this.panel1.Controls.Add(this.TextW);
+            this.panel1.Controls.Add(this.TextY);
+            this.panel1.Controls.Add(this.TextX);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.Player1_cBoxScreen);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 31);
+            this.panel1.TabIndex = 5;
+            // 
+            // TextH
+            // 
+            this.TextH.Location = new System.Drawing.Point(497, 6);
+            this.TextH.Name = "TextH";
+            this.TextH.Size = new System.Drawing.Size(68, 20);
+            this.TextH.TabIndex = 12;
+            this.TextH.Text = "2000";
+            this.TextH.TextChanged += new System.EventHandler(this.TextH_TextChanged);
+            // 
+            // TextW
+            // 
+            this.TextW.Location = new System.Drawing.Point(423, 6);
+            this.TextW.Name = "TextW";
+            this.TextW.Size = new System.Drawing.Size(68, 20);
+            this.TextW.TabIndex = 11;
+            this.TextW.Text = "4000";
+            this.TextW.TextChanged += new System.EventHandler(this.TextW_TextChanged);
+            // 
+            // TextY
+            // 
+            this.TextY.Location = new System.Drawing.Point(316, 6);
+            this.TextY.Name = "TextY";
+            this.TextY.Size = new System.Drawing.Size(68, 20);
+            this.TextY.TabIndex = 10;
+            this.TextY.Text = "50";
+            this.TextY.TextChanged += new System.EventHandler(this.TextY_TextChanged);
+            // 
+            // TextX
+            // 
+            this.TextX.Location = new System.Drawing.Point(242, 6);
+            this.TextX.Name = "TextX";
+            this.TextX.Size = new System.Drawing.Size(68, 20);
+            this.TextX.TabIndex = 9;
+            this.TextX.Text = "430";
+            this.TextX.TextChanged += new System.EventHandler(this.TextX_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(724, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "COLOSE";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(643, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "START";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // Player1_cBoxScreen
+            // 
+            this.Player1_cBoxScreen.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.Player1_cBoxScreen.FormattingEnabled = true;
+            this.Player1_cBoxScreen.Location = new System.Drawing.Point(77, 6);
+            this.Player1_cBoxScreen.Name = "Player1_cBoxScreen";
+            this.Player1_cBoxScreen.Size = new System.Drawing.Size(124, 21);
+            this.Player1_cBoxScreen.TabIndex = 6;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(3, 6);
+            this.textBox1.MaxLength = 2;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(68, 20);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.Text = "2";
+            this.textBox1.LocationChanged += new System.EventHandler(this.TextBox1_LocationChanged);
+            this.textBox1.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextBox1_KeyPress);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.ClientSize = new System.Drawing.Size(800, 34);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "ფორმულა ზუმი";
+            this.TopMost = true;
+            this.TransparencyKey = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox Player1_cBoxScreen;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox TextH;
+        private System.Windows.Forms.TextBox TextW;
+        private System.Windows.Forms.TextBox TextY;
+        private System.Windows.Forms.TextBox TextX;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Zumi
+{
+    public partial class Form1 : Form
+    {
+        private Bitmap Bit;
+        private Graphics Graf;
+        private int Zoom = 2;
+
+        //bool isDrag = false;
+        //Rectangle theRectangle = new Rectangle(new Point(0, 0), new Size(0, 0));
+        //Point startPoint;
+        //Point currPoint;
+
+        public Size SSize;
+
+        public System.Windows.Forms.Screen[] _screens;
+
+       
+        private Form2 zooomForm;
+
+        public Bitmap Bit1 { get => Bit; set => Bit = value; }
+        public Graphics Graf1 { get => Graf; set => Graf = value; }
+        public Form2 ZooomForm { get => zooomForm; set => zooomForm = value; }
+
+        //ZooomForm = new Form2();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            _screens = System.Windows.Forms.Screen.AllScreens;
+            foreach (var screen in _screens)
+            {
+                Player1_cBoxScreen.Items.Add(screen.DeviceName);
+
+            }
+            Player1_cBoxScreen.SelectedItem = Player1_cBoxScreen.Items[Player1_cBoxScreen.Items.Count - 1];
+            ZooomForm = new Form2();
+            //ZooomForm.DesktopLocation = new Point(300,100);
+            
+
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            //Bit = new Bitmap(Pcb_Foto.Width / Zoom, Pcb_Foto.Height / Zoom, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+            Bit1 = new Bitmap(ZooomForm.Pcb_Foto2.Width / Zoom, ZooomForm.Pcb_Foto2.Height / Zoom, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+            Graf1 = Graphics.FromImage(Bit1);
+            //currPoint = this.Pcb_Foto.PointToScreen(new Point());
+
+            SSize = new Size(Convert.ToInt32(TextW.Text), Convert.ToInt32(TextH.Text));
+            Graf1.CopyFromScreen(Convert.ToInt32(TextX.Text) , Convert.ToInt32(TextY.Text)  , ZooomForm.Pcb_Foto2.Location.X, ZooomForm.Pcb_Foto2.Location.Y, SSize, System.Drawing.CopyPixelOperation.SourceCopy);
+            //Graf.CopyFromScreen(Convert.ToInt32(TextX.Text) - ZooomForm.Pcb_Foto2.Width / (Zoom * 2), Convert.ToInt32(TextY.Text) - ZooomForm.Pcb_Foto2.Height / (Zoom * 2), 0, 0, SSize, System.Drawing.CopyPixelOperation.SourceCopy);
+            //            Graf.CopyFromScreen(MousePosition.X - ZooomForm.Pcb_Foto2.Width / (Zoom * 2), MousePosition.Y - ZooomForm.Pcb_Foto2.Height / (Zoom * 2), 0, 0, ZooomForm.Pcb_Foto2.Size, System.Drawing.CopyPixelOperation.SourceCopy);
+            //            Graf.CopyFromScreen((currPoint.X + this.Pcb_Foto.Width)  - ZooomForm.Pcb_Foto2.Width / (Zoom * 2), (currPoint.Y + this.Pcb_Foto.Height) - ZooomForm.Pcb_Foto2.Height / (Zoom * 2), 0, 0, ZooomForm.Pcb_Foto2.Size, System.Drawing.CopyPixelOperation.SourceCopy);
+
+            
+            //Graf.CopyFromScreen(Convert.ToInt32(TextX.Text) - ZooomForm.Pcb_Foto2.Width / (Zoom * 2), (Convert.ToInt32(TextY.Text) + this.Pcb_Foto.Height) - ZooomForm.Pcb_Foto2.Height / (Zoom * 2), 0, 0, SSize, System.Drawing.CopyPixelOperation.SourceCopy);
+            //Console.WriteLine("Mouse Pos: " + Convert.ToString(MousePosition.X) + "," + Convert.ToString(MousePosition.Y));
+            //Console.WriteLine("Pcb_Foto Pos: " + Convert.ToString(currPoint.X) + "," + Convert.ToString(currPoint.Y));
+
+            ZooomForm.Pcb_Foto2.Image = Bit1;
+
+
+            /*
+             * 
+             * 
+            //Bit = new Bitmap(ZooomForm.Pcb_Foto2.Width / Zoom, ZooomForm.Pcb_Foto2.Height / Zoom, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+            Bit = new Bitmap(ZooomForm.Pcb_Foto2.Width, ZooomForm.Pcb_Foto2.Height, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+            Graf = Graphics.FromImage(Bit);
+            
+            //Graf.CopyFromScreen(currPoint.X / (Zoom * 2), currPoint.Y / (Zoom * 2), 0, 0, this.Pcb_Foto.Size, System.Drawing.CopyPixelOperation.SourceCopy);
+            Graf.CopyFromScreen(MousePosition.X - Pcb_Foto.Width / (Zoom * 2), MousePosition.Y - Pcb_Foto.Height / (Zoom * 2), 0, 0, Pcb_Foto.Size, System.Drawing.CopyPixelOperation.SourceCopy);
+            //Pcb_Foto.Image = Bit;
+            ZooomForm.Pcb_Foto2.Image = Bit;
+
+
+            */
+
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            //ZooomForm = new Form();
+
+
+            // ZooomForm.Show();
+
+            // ZooomForm.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            //ZooomForm.WindowState = System.Windows.Forms.FormWindowState.Normal;
+            //ZooomForm.Location = _screens[Player1_cBoxScreen.SelectedIndex].WorkingArea.Location;
+            ZooomForm.Show();
+            SetFormLocation(ZooomForm, _screens[Player1_cBoxScreen.SelectedIndex]);
+
+            
+
+        }
+
+        private void SetFormLocation(Form form, Screen screen)
+        {
+            // first method
+            Rectangle bounds = screen.Bounds;
+            form.SetBounds(bounds.X, bounds.Y, bounds.Width, bounds.Height);
+
+            // second method
+            //Point location = screen.Bounds.Location;
+            //Size size = screen.Bounds.Size;
+
+            //form.Left = location.X;
+            //form.Top = location.Y;
+            //form.Width = size.Width;
+            //form.Height = size.Height;
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            /*
+            if (e.Button == MouseButtons.Left)
+            {
+                isDrag = true;
+            }
+
+            Control control = (Control)sender;
+
+            // Calculate the startPoint by using the PointToScreen 
+            // method.
+            startPoint = control.PointToScreen(new Point(e.X, e.Y));
+            textBox1.Text = startPoint.ToString();
+            */
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            /*
+            if (isDrag)
+
+            // Hide the previous rectangle by calling the 
+            // DrawReversibleFrame method with the same parameters.
+            {
+                ControlPaint.DrawReversibleFrame(theRectangle,
+                    this.BackColor, FrameStyle.Dashed);
+
+                // Calculate the endpoint and dimensions for the new 
+                // rectangle, again using the PointToScreen method.
+                Point endPoint = ((Control)sender).PointToScreen(new Point(e.X, e.Y));
+
+                int width = endPoint.X - startPoint.X;
+                int height = endPoint.Y - startPoint.Y;
+                theRectangle = new Rectangle(startPoint.X,
+                    startPoint.Y, width, height);
+
+                // Draw the new rectangle by calling DrawReversibleFrame
+                // again.  
+                ControlPaint.DrawReversibleFrame(theRectangle,
+                    this.BackColor, FrameStyle.Dashed);
+            }
+            */
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            /*
+            isDrag = false;
+
+            // Draw the rectangle to be evaluated. Set a dashed frame style 
+            // using the FrameStyle enumeration.
+            ControlPaint.DrawReversibleFrame(theRectangle,
+                this.BackColor, FrameStyle.Dashed);
+
+            // Find out which controls intersect the rectangle and 
+            // change their color. The method uses the RectangleToScreen  
+            // method to convert the Control's client coordinates 
+            // to screen coordinates.
+            Rectangle controlRectangle;
+            for (int i = 0; i < Controls.Count; i++)
+            {
+                controlRectangle = Controls[i].RectangleToScreen
+                    (Controls[i].ClientRectangle);
+                if (controlRectangle.IntersectsWith(theRectangle))
+                {
+                    Controls[i].BackColor = Color.BurlyWood;
+                }
+            }
+
+            // Reset the rectangle.
+            theRectangle = new Rectangle(0, 0, 0, 0);
+            */
+        }
+
+        private void TextBox1_LocationChanged(object sender, EventArgs e)
+        {
+            //currPoint = this.Pcb_Foto.PointToScreen(new Point());
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+            Zoom = (textBox1.Text != "") ? Convert.ToInt32(textBox1.Text) : 2;
+            
+        }
+
+        private void TextX_TextChanged(object sender, EventArgs e)
+        {
+           // DrawRectangleFloat();
+        }
+        public void DrawRectangleFloat()
+        {
+            /*
+            // Create pen.
+            Graphics g = this.CreateGraphics();
+
+            //Pen blackPen = new Pen(Color.Black, 3);
+
+            // Create location and size of rectangle.
+            
+            int x = Convert.ToInt32(TextX.Text);
+            int y = Convert.ToInt32(TextY.Text);
+            int width = Convert.ToInt32(TextW.Text);
+            int height = Convert.ToInt32(TextH.Text);
+
+            // Draw rectangle to screen.
+            
+
+
+            
+            Pen selPen = new Pen(Color.Blue);
+            g.DrawRectangle(selPen, x, y, width, height);
+            g.Dispose();
+            */
+        }
+
+        private void TextY_TextChanged(object sender, EventArgs e)
+        {
+           // DrawRectangleFloat();
+        }
+
+        private void TextW_TextChanged(object sender, EventArgs e)
+        {
+            //DrawRectangleFloat();
+        }
+
+        private void TextH_TextChanged(object sender, EventArgs e)
+        {
+            //DrawRectangleFloat();
+        }
+
+        private void TextBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+        }
+    }
+    }
+
